373c36f9a5a4051e3b6680708632d08c9170bff4
Nick Poorman <nickpoorman@gmail.com>
2019-06-24 22:30:12 -0400

    Updated bullseye docs

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ Package bullseye provides an implementation of a DataFrame using Apache Arrow.
 Basics
 
 The DataFrame is an immutable heterogeneous tabular data structure with labeled columns.
-It stores it's data in using a provided Arrow Allocator, using the fundamental data
+It stores it's raw bytes using a provided Arrow Allocator by using the fundamental data
 structure of Array (columns), which holds a sequence of values of the same type. An array
 consists of memory holding the data and an additional validity bitmap that indicates if
 the corresponding entry in the array is valid (not null).
