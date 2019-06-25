b3b1fe45ca65c40d4b42190ca60e06d9e24fcd4d
Nick Poorman <nickpoorman@gmail.com>
2019-06-24 22:10:47 -0400

    Move example for docs

--- dataframe/dataframe_test.go
+++ dataframe/dataframe_test.go
@@ -1,7 +1,6 @@
 package dataframe
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -1257,22 +1256,4 @@ rec[0]["col2-f64-x2"]: [2 4 6 8 10 12 14 16 18 20]
 	if &df == &df2 {
 		t.Fatalf("references are the same. df is not a copy of df2 (%v) == (%v)", &df, &df2)
 	}
-}
-
-func TestExample(t *testing.T) {
-	pool := memory.NewGoAllocator()
-	df, _ := NewDataFrameFromMem(pool, Dict{
-		"col1": []int32{1, 2, 3, 4, 5},
-		"col2": []float64{1.1, 2.2, 3.3, 4.4, 5},
-		"col3": []string{"foo", "bar", "ping", "", "pong"},
-		"col4": []interface{}{2, 4, 6, nil, 8},
-	})
-	defer df.Release()
-	fmt.Printf("DataFrame:\n%s\n", df.Display(0))
-
-	// DataFrame:
-	// rec[0]["col1"]: [1 2 3 4 5]
-	// rec[0]["col2"]: [1.1 2.2 3.3 4.4 5]
-	// rec[0]["col3"]: ["foo" "bar" "ping" "" "pong"]
-	// rec[0]["col4"]: [2 4 6 (null) 8]
 }
---
+++ dataframe/example_test.go
@@ -0,0 +1,29 @@
+package dataframe_test
+
+import (
+	"fmt"
+
+	"github.com/apache/arrow/go/arrow/memory"
+	"github.com/go-bullseye/bullseye/dataframe"
+)
+
+// This example demonstrates creating a new DataFrame from memory
+// using a Dict.
+func Example_newDataFrameFromMemory() {
+	pool := memory.NewGoAllocator()
+	df, _ := dataframe.NewDataFrameFromMem(pool, dataframe.Dict{
+		"col1": []int32{1, 2, 3, 4, 5},
+		"col2": []float64{1.1, 2.2, 3.3, 4.4, 5},
+		"col3": []string{"foo", "bar", "ping", "", "pong"},
+		"col4": []interface{}{2, 4, 6, nil, 8},
+	})
+	defer df.Release()
+	fmt.Printf("DataFrame:\n%s\n", df.Display(0))
+
+	// Output:
+	// DataFrame:
+	// rec[0]["col1"]: [1 2 3 4 5]
+	// rec[0]["col2"]: [1.1 2.2 3.3 4.4 5]
+	// rec[0]["col3"]: ["foo" "bar" "ping" "" "pong"]
+	// rec[0]["col4"]: [2 4 6 (null) 8]
+}
