9bae68d708532705b5c644b5dc58c266c5d2f5cd
Nick Poorman <nickpoorman@gmail.com>
2019-06-24 22:13:28 -0400

    Updated iterator/doc

--- iterator/doc.go
+++ iterator/doc.go
@@ -1,7 +1,7 @@
 /*
 Package iterator provides iterators for chunks and values.
 
-Since Arrow can store chunks larger than tha max int64 (9223372036854775807) due to how it
+Since Arrow can store chunks larger than the max int64 (9223372036854775807) due to how it
 store chunks, it's best to use iterators to iterate over chunks and their values.
 
 There are generic ChunkIterator and ValueIterator implementations as well as specific
