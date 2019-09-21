db275a91e67c7dcb6b8b316f7b50595a83f1332e
Nick Poorman <nickpoorman@gmail.com>
2019-09-21 13:48:19 -0400

    Added all the types

--- dataframe/json.go
+++ dataframe/json.go
@@ -50,20 +50,72 @@ func rowToJSON(schema *arrow.Schema, values []interface{}) (map[string]interface
 func fieldToJSON(obj map[string]interface{}, field arrow.Field, value interface{}) error {
 	name := field.Name
 	switch field.Type.ID() {
-	case arrow.STRUCT:
-		// This one we need to loop over
-		panic("not implemented")
-	case arrow.LIST:
-		// This one we need to loop over
-		panic("not implemented")
+	case arrow.NULL:
+		obj[name] = nil
 	case arrow.BOOL:
+		obj[name] = value
+	case arrow.UINT8:
+		obj[name] = value
+	case arrow.INT8:
+		obj[name] = value
+	case arrow.UINT16:
+		obj[name] = value
+	case arrow.INT16:
+		obj[name] = value
+	case arrow.UINT32:
 		obj[name] = value
 	case arrow.INT32:
 		obj[name] = value
+	case arrow.UINT64:
+		obj[name] = value
+	case arrow.INT64:
+		obj[name] = value
+	case arrow.FLOAT16:
+		// TODO(nickpoorman): Verify we don't need to do anything to this.
+		obj[name] = value
+	case arrow.FLOAT32:
+		obj[name] = value
 	case arrow.FLOAT64:
 		obj[name] = value
+	case arrow.STRING:
+		obj[name] = value
+	case arrow.BINARY:
+		obj[name] = value
+	case arrow.FIXED_SIZE_BINARY:
+		panic("not implemented")
+		// TODO: Convert to string?
+	case arrow.DATE32:
+		panic("not implemented")
+	case arrow.DATE64:
+		panic("not implemented")
+	case arrow.TIMESTAMP:
+		panic("not implemented")
+	case arrow.TIME32:
+		panic("not implemented")
+	case arrow.TIME64:
+		panic("not implemented")
+	case arrow.INTERVAL:
+		panic("not implemented")
+	case arrow.DECIMAL:
+		panic("not implemented")
+	case arrow.LIST:
+		panic("not implemented")
+	case arrow.STRUCT:
+		panic("not implemented")
+	case arrow.UNION:
+		panic("not implemented")
+	case arrow.DICTIONARY:
+		panic("not implemented")
+	case arrow.MAP:
+		panic("not implemented")
+	case arrow.EXTENSION:
+		panic("not implemented")
+	case arrow.FIXED_SIZE_LIST:
+		panic("not implemented")
+	case arrow.DURATION:
+		panic("not implemented")
 	default:
-		panic(fmt.Sprintf("type not supported: %s", field.Type.Name()))
+		panic(fmt.Sprintf("unknown type: %s", field.Type.Name()))
 	}
 
 	return nil
--- dataframe/json_test.go
+++ dataframe/json_test.go
@@ -1,10 +1,24 @@
 package dataframe
 
 import (
-	"os"
+	"bytes"
 	"testing"
 
 	"github.com/apache/arrow/go/arrow/memory"
+)
+
+var (
+	toJSONResult = `{"col1-i32":1,"col2-f64":1}
+{"col1-i32":2,"col2-f64":2}
+{"col1-i32":3,"col2-f64":3}
+{"col1-i32":4,"col2-f64":4}
+{"col1-i32":5,"col2-f64":5}
+{"col1-i32":6,"col2-f64":6}
+{"col1-i32":7,"col2-f64":7}
+{"col1-i32":8,"col2-f64":8}
+{"col1-i32":9,"col2-f64":9}
+{"col1-i32":10,"col2-f64":10}
+`
 )
 
 func TestToJSON(t *testing.T) {
@@ -20,19 +34,26 @@ func TestToJSON(t *testing.T) {
 	}
 	defer df.Release()
 
-	f, err := os.Create("/tmp/dataframe-test.json")
+	// f, err := os.Create("/tmp/dataframe-test.json")
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// defer f.Close()
+	// defer func() {
+	// 	err := f.Sync()
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// }()
+
+	var b bytes.Buffer
+
+	err = df.ToJSON(&b)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Close()
 
-	err = df.ToJSON(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = f.Sync()
-	if err != nil {
-		t.Fatal(err)
+	if got, want := b.String(), toJSONResult; got != want {
+		t.Fatalf("got=%s, want=%s", got, want)
 	}
 }
